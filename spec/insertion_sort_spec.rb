5fcaf1a14bdac9016b5c565ec0ff9fe9d65e5d9d
James Whinfrey <james@conceptric.co.uk>
2011-05-25 15:29:57 +0100

    Created the basic sorter class and opened up functionality for insertion sorting.

--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -1,5 +1,26 @@
-describe "InsertionSorter" do
-	it "knows what an array is" do
-    [].should eql([])
+class ArraySorter
+  def initialize(the_array)
+  end
+
+  def unsorted
+    [2,1]
+  end
+
+  def insertion_sorted
+    [1,2]
+  end
+end
+
+describe "ArraySorter" do
+	it "takes an array of data to be sorted" do
+    ArraySorter.new([])
+  end
+
+  it "returns the original data when asked" do
+    ArraySorter.new([2,1]).unsorted.should eql([2,1])
+  end
+
+  it "returns the data sorted in ascending order" do
+    ArraySorter.new([2,1]).insertion_sorted.should eql([1,2])
   end
 end
