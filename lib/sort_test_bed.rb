1a1edd81b72bc54e8698a65a0098bbd9b7b85d3c
James Whinfrey <james@conceptric.co.uk>
2011-06-08 10:56:16 +0100

    Adds a starting time variable for the test bed.

--- lib/sort_test_bed.rb
+++ lib/sort_test_bed.rb
@@ -2,12 +2,15 @@ require_relative 'integer_array_sort'
 
 class SortTestBed
   attr_reader :unsorted
+  attr_reader :start_time
 
   def initialize(the_array)
-    @unsorted = the_array
+    @unsorted = the_array           
+    @start_time = nil
   end
 
-  def insertion_sorted
+  def insertion_sorted 
+    @start_time = Time.now
     IntegerArraySort::insertion_sort(@unsorted)
   end
 end
--- spec/sort_test_bed_spec.rb
+++ spec/sort_test_bed_spec.rb
@@ -7,14 +7,22 @@ describe "SortTestBed" do
   end
 
   it "returns the original data when asked" do
-    SortTestBed.new([3,1]).
-      unsorted.should eql([3,1])
+    SortTestBed.new([3,1]).unsorted.should eql([3,1])
   end
-                       
+
   it "uses the insertion sort method of IntegerArraySort" do
     IntegerArraySort.should_receive(:insertion_sort).once.
     with([2,1])
     SortTestBed.new([2,1]).insertion_sorted
   end
 
+  it "records a timestamp when the sort method is called" do
+    test_bed = SortTestBed.new([2,1])
+    test_bed.start_time.should be_nil
+    test_bed.insertion_sorted    
+    test_bed.start_time.should_not be_nil
+  end
+  it "records a timestamp when the sort method is returns"
+  it "returns the sort duration in milliseconds"
+  
 end
